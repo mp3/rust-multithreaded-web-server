8617da709ef355189ad3540e5c164501becfc13c
mp3 <im.in.a.coma.xxx@gmail.com>
2019-02-24 11:58:33 +0900

    turning single-threaded server into a multithreaded server

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,4 @@
+use rust_multithreaded_web_server::ThreadPool;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
@@ -7,11 +8,14 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
