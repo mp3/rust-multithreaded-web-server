9b95e5421a0727d5b85d1ce2f77b1a49520a9620
mp3 <im.in.a.coma.xxx@gmail.com>
2019-02-24 14:33:07 +0900

    signaling to the threads to stop listening for jobs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,5 @@
 use rust_multithreaded_web_server::ThreadPool;
+
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
@@ -10,13 +11,15 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
